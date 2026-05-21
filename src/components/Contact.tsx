a479cd50b8b1a7d8a7f3eee08f0ff6f597fad19a
Grégoire Pelizzardi <gregpeli@yahoo.fr>
2026-05-21 11:36:18 +0200

    ajout de formspree

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -221,7 +221,7 @@ export default function Contact() {
 
     setStatus("loading");
     try {
-      const res = await fetch("/api/contact", {
+      const res = await fetch("https://formspree.io/f/mqeykljd", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(form),
